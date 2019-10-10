186d5acc85753301595ba10d94a3740271423ab3
Jack Butterfield <jack.butterfield@student.manchester.ac.uk>
2019-10-10 20:20:20 +0100

    first attempt at the test

---
+++ tests/games/stendhal/server/entity/SummonPetScrollsTest.java
@@ -0,0 +1,51 @@
+package games.stendhal.server.entity;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import java.util.HashMap;
+//import games.stendhal.client.World;
+import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.entity.creature.PurpleDragon;
+import games.stendhal.server.entity.item.scroll.BlankPetScroll;
+import games.stendhal.server.entity.item.scroll.SummonPetScroll;
+import marauroa.common.game.RPObject;
+
+
+public class SummonPetScrollsTest {
+	
+	@Test
+	public void testSummon() {
+		
+		//World testWorld = new World();
+		final Player testPlayer = new Player(new RPObject());
+		PurpleDragon testDragon = new PurpleDragon(testPlayer);
+		
+		BlankPetScroll testBlankScroll = new BlankPetScroll("test string", "test string", "test string",
+				new HashMap<String,String>());
+		
+		
+		testDragon.setWeight(12345);
+		String initialDescribe = testDragon.describe();
+		
+		testBlankScroll.useScroll(testPlayer);
+		
+		SummonPetScroll testSummonScroll = new SummonPetScroll("test string", "test string", "test string",
+				new HashMap<String,String>());
+		testSummonScroll.dropBlank(testPlayer);
+		
+		String newDescribe = testDragon.describe();
+		
+		assertEquals("checks that weights etc will be the same",initialDescribe, newDescribe);
+		
+		
+		
+		/*
+		initialPet
+		returnedPet
+		scroll
+		world
+		*/
+	}
+
+}
