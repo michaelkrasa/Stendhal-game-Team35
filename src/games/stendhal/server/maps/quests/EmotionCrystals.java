d26e75ed1bc6b9ed84f0f740469c0041c761ba65
h49199at <alexandru.tentes@student.manchester.ac.uk>
2019-10-15 00:46:28 +0100

    made the List which stores the crystals for the travel log be static

--- src/games/stendhal/server/maps/quests/EmotionCrystals.java
+++ src/games/stendhal/server/maps/quests/EmotionCrystals.java
@@ -85,7 +85,7 @@ public class EmotionCrystals extends AbstractQuest {
 	private static final int OFFSET_TIMESTAMPS = 1;
 	private static final int OFFSET_SUCCESS_MARKER = 6;
 	
-	public List<String> gatheredCrystals = new ArrayList<String>();
+	public static List<String> gatheredCrystals = new ArrayList<String>();
 
 	@Override
 	public List<String> getHistory(final Player player) {
