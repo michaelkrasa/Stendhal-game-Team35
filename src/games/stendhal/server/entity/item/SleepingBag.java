764e487bfa6ead995f31ee2debf0601c7f636d4e
Misha <michael.krasa@student.manchester.ac.uk>
2019-11-11 12:08:08 +0000

    Basic structure for Sleeping bag ready - sprite, xml, item class

---
+++ src/games/stendhal/server/entity/item/SleepingBag.java
@@ -0,0 +1,9 @@
+package games.stendhal.server.entity.item;
+
+public class SleepingBag extends AreaUseItem {
+
+	public SleepingBag(AreaUseItem item) {
+		super(item);
+		// TODO Auto-generated constructor stub
+	}
+}
